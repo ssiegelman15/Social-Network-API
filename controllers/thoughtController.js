e2488550f8330db2d131e0da2c533a692ef78a23
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-06-28 11:02:02 -0400

    modified code to find all thought or one thought by ID

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,27 +1,31 @@
-const { Course, Student } = require('../models');
+const { User, Thought, Reaction } = require("../models");
 
 module.exports = {
-  // Get all courses
-  getCourses(req, res) {
-    Course.find()
-      .then((courses) => res.json(courses))
+  // Get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
-  // Get a course
-  getSingleCourse(req, res) {
-    Course.findOne({ _id: req.params.courseId })
-      .select('-__v')
-      .then((course) =>
-        !course
-          ? res.status(404).json({ message: 'No course with that ID' })
-          : res.json(course)
+
+  // Get a Thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res
+              .status(404)
+              .json({ message: "No thought was found with that ID!" })
+          : res.json(thought)
       )
       .catch((err) => res.status(500).json(err));
   },
-  // Create a course
-  createCourse(req, res) {
-    Course.create(req.body)
-      .then((course) => res.json(course))
+
+  // Create a thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
       .catch((err) => {
         console.log(err);
         return res.status(500).json(err);
@@ -32,10 +36,10 @@ module.exports = {
     Course.findOneAndDelete({ _id: req.params.courseId })
       .then((course) =>
         !course
-          ? res.status(404).json({ message: 'No course with that ID' })
+          ? res.status(404).json({ message: "No course with that ID" })
           : Student.deleteMany({ _id: { $in: course.students } })
       )
-      .then(() => res.json({ message: 'Course and students deleted!' }))
+      .then(() => res.json({ message: "Course and students deleted!" }))
       .catch((err) => res.status(500).json(err));
   },
   // Update a course
@@ -47,7 +51,7 @@ module.exports = {
     )
       .then((course) =>
         !course
-          ? res.status(404).json({ message: 'No course with this id!' })
+          ? res.status(404).json({ message: "No course with this id!" })
           : res.json(course)
       )
       .catch((err) => res.status(500).json(err));
