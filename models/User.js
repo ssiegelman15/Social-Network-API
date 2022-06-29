eb4f7194e3154881a6f8eb3451f7ce5ceb9c434c
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-06-29 09:03:33 -0400

    updated model names

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-const { Schema, model } = require('mongoose');
-const assignmentSchema = require('./Assignment');
+const { Schema, model } = require("mongoose");
+const assignmentSchema = require("./Reaction");
 
 // Schema to create Student model
 const studentSchema = new Schema(
@@ -28,6 +28,6 @@ const studentSchema = new Schema(
   }
 );
 
-const Student = model('student', studentSchema);
+const Student = model("student", studentSchema);
 
 module.exports = Student;
