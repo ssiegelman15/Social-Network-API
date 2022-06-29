abbb9296c580be559ff2e6f32f487d627539f811
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-06-29 09:13:49 -0400

    created User model to match requirements in assignment readme

--- models/User.js
+++ models/User.js
@@ -2,32 +2,45 @@ const { Schema, model } = require("mongoose");
 const assignmentSchema = require("./Reaction");
 
 // Schema to create Student model
-const studentSchema = new Schema(
+const userSchema = new Schema(
   {
-    first: {
+    username: {
       type: String,
+      unique: true,
       required: true,
-      max_length: 50,
+      trim: true,
     },
-    last: {
+    email: {
       type: String,
+      unique: true,
       required: true,
-      max_length: 50,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
     },
-    github: {
-      type: String,
-      required: true,
-      max_length: 50,
-    },
-    assignments: [assignmentSchema],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     toJSON: {
-      getters: true,
+      virtuals: true,
     },
+    id: false,
   }
 );
 
-const Student = model("student", studentSchema);
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
-module.exports = Student;
+const User = model("user", userSchema);
+
+module.exports = User;
